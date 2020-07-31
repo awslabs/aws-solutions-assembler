dbe4d94927df7d85fcb8e213e243432e52c9eba0
cc4i <chuancc@gmail.com>
2020-07-31 21:54:22 +1200

    chore: add missing files for ui

---
+++ ui/src/pages/const.ts
@@ -0,0 +1,8 @@
+export const EN_TITLE = "AWS Solutions Assembler - AWS Prototyping";
+export const CN_TITLE = "AWS Solutions Assembler - AWS Prototyping";
+export const SOCKET_PATH = "ws://127.0.0.1:9090";
+export const GITHUB_LINK = "https://github.com/awslabs/aws-solutions-assembler";
+export const DOC_EN_LINK =
+  "https://wchaws.github.io/aws-solutions-assembler/en/getting-started/simple-usage/";
+export const DOC_CN_LINK =
+  "https://wchaws.github.io/aws-solutions-assembler/zh/getting-started/simple-usage/";
